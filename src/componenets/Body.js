74565bb0182803c34868020e1b23689aeda1ac7b
devesh-434 <142976811+devesh-434@users.noreply.github.com>
2023-10-16 14:57:10 +0530

    Add files via upload

---
+++ src/componenets/Body.js
@@ -0,0 +1,154 @@
+import "./Body.css"
+
+export function Body() {
+  const BodyStyle = {
+    width: "100%",
+    minHeight: "60vh",
+    background: "linear-gradient(135deg, #e74c3c 0%, #f1c40f 35%, #8e44ad 100%), linear-gradient(#0088ff, green, black 100vh)"
+
+
+  }
+  return (
+    <div style={BodyStyle}>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L48,117.3C96,139,192,181,288,176C384,171,480,117,576,96C672,75,768,85,864,101.3C960,117,1056,139,1152,154.7C1248,171,1344,181,1392,186.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
+      <div className="bg-svg-blue p-5 text-center text-light">
+        <h1>Devesh Khandelwal</h1>
+        <div className="h4">Vellore Institute of Technology</div>
+        <div className="h4">
+          Email: <a href="/#">devesh.khandelwal2021@vitstudent.ac.in</a>
+        </div>
+        <div className="h4">
+          LinkedIn:{" "}
+          <a
+            href="https://www.linkedin.com/in/devesh-khandelwal-a0ab04205/"
+            target="blank"
+          >
+            https://www.linkedin.com/in/devesh-khandelwal-a0ab04205/
+          </a>
+        </div>
+        <div className="h4">
+          GitHub:{" "}
+          <a href="https://github.com/devesh-434" target="blank">
+          https://github.com/devesh-434
+          </a>
+        </div>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L80,128C160,160,320,224,480,213.3C640,203,800,117,960,90.7C1120,64,1280,96,1360,112L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L48,117.3C96,139,192,181,288,176C384,171,480,117,576,96C672,75,768,85,864,101.3C960,117,1056,139,1152,154.7C1248,171,1344,181,1392,186.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
+      <div className="d-flex justify-content-center px-5 ">
+        <div className="text-center text-light px-5 ">
+          <h1>About</h1>
+          <div>
+          Ambitious 3rd-year engineering student specializing in web development, seeking work opportunities and internships. Strong problemsolving skills, excellent communication, and a passion for creating innovative web solutions. Eager to contribute to a dynamic team
+          and expand skills
+          </div>
+        </div>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L80,128C160,160,320,224,480,213.3C640,203,800,117,960,90.7C1120,64,1280,96,1360,112L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L48,117.3C96,139,192,181,288,176C384,171,480,117,576,96C672,75,768,85,864,101.3C960,117,1056,139,1152,154.7C1248,171,1344,181,1392,186.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
+      <div className="d-flex justify-content-center px-5">
+        <div className="text-center text-light px-5 ">
+          <h1>Skills</h1>
+          <div>
+          <ul>
+          <li> <h5> Data Structures  📊</h5></li>
+          <li> <h5> Algorithms  🧩</h5></li>
+          <li> <h5> C Programming  🔍</h5></li>
+          <li> <h5> C++ Programming  🔢</h5></li>
+          <li> <h5> Java Programming  ☕</h5></li>
+          <li> <h5> HTML  🌐</h5></li>
+          <li> <h5> CSS  🎨</h5></li>
+          <li> <h5> Javascript  🚀</h5></li>
+          <li> <h5> Python  🐍</h5></li>
+          <li> <h5> Oracle DB  🔶</h5></li>
+          </ul>
+          </div>
+        </div>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L80,128C160,160,320,224,480,213.3C640,203,800,117,960,90.7C1120,64,1280,96,1360,112L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L48,117.3C96,139,192,181,288,176C384,171,480,117,576,96C672,75,768,85,864,101.3C960,117,1056,139,1152,154.7C1248,171,1344,181,1392,186.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
+      <div className="d-flex justify-content-center px-5">
+        <div className="text-center text-light px-5">
+          <h1>Education</h1>
+          <div className="p-5">
+            <h2>Vellore Institute of Technology</h2>
+            <h4>Bachelor of Technology - BTech, Computer Science with spl in Blockchain Technology</h4>
+            <h4>Vellore, Tamil Nadu, India</h4>
+          </div>
+
+          <div className="p-5">
+            <h2>Harvest International School, Bangalore</h2>
+            <h4>12th Grade CBSE</h4>
+            <h4>Bangalore, Karnataka, India</h4>
+          </div>
+        </div>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L80,128C160,160,320,224,480,213.3C640,203,800,117,960,90.7C1120,64,1280,96,1360,112L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  )
+}
+export default Body
